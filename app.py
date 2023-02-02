fa30249e533a43c1a1b915457383378dd1839c5f
Leendert Hayen <lmhayen@ncsu.edu>
2023-02-02 12:29:16 +0100

    Bug fixes, functional beta+ and databases

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import csv
 import numpy as np
 import pandas as pd
+import re
 import thecobs.spectral_functions as sf
 import thecobs.constants as constants
 
@@ -25,11 +26,30 @@ st.markdown("""
 st.sidebar.markdown("## Set the decay parameters")
 
 select_event = st.sidebar.selectbox('How do you want to find data?',
-                                    ['Manually', 'Query NNDC'])
+                                    ['Manually', 'Database'])
+
+def approxNuclRadius(Z, N):
+    '''
+    Return nuclear charge radius in fm.
+    Taken from Chinese Physics C Vol. 46, No. 7 (2022) 074105'''
+    if N > 0:
+        rN = 1.629
+        b = 0.451
+        return rN*(1-b*(N-Z)/N)*N**(1/3)*(3/5)**0.5
+    else:
+        rA = 1.282
+        b = 0.342
+        return rA*(1-b*(N-Z)/(Z+N))*(Z+N)**(1/3)*(3/5)**0.5
+
+def stableA(Z):
+    aC = 0.71 # MeV
+    aA = 23.7 # MeV
+    alpha = aC/4/aA
+
+    return 2*Z*(1+alpha*(2*Z)**(2/3))
 
 @st.cache
 def downloadAME():
-    UMASSC2 = 931.49410242
     names = ['N', 'Z', 'A', 'name', 'mass']
     data = []
     url = 'https://www-nds.iaea.org/amdc/ame2020/mass_1.mas20.txt'
@@ -50,39 +70,47 @@ def downloadAME():
             atomicMassString = line[106:121]
             atomicMassString = atomicMassString[:3] + atomicMassString[4:]
             atomicMassString = atomicMassString.replace('#', '.')
-            atomicMass = float(atomicMassString)*1e-6*UMASSC2
+            atomicMass = float(atomicMassString)*1e-6*constants.U_MASS_C2
             data.append([n, z, a, name, atomicMass])
     return pd.DataFrame(data, columns=names)
 
-if select_event == 'Manually':
-    beta_type = st.sidebar.selectbox('Type of beta transition',
+beta_type = st.sidebar.selectbox('Type of beta transition',
                                     ['Beta-', 'Beta+'])
 
+Qvalue = 1000.
+
+if select_event == 'Manually':
     sl_z = st.sidebar.number_input('Proton number', min_value=1, max_value=120, step=1, help="Set the proton number of the initial state")
-    sl_a = st.sidebar.number_input('Mass number', min_value=sl_z, max_value=120, step=1, help="Set the mass number of the initial state")
-    sl_r = st.sidebar.number_input('Radius', min_value=0.01, max_value=100., step=0.1, value=1.2, help="Set the rms nuclear radius in fm")
-    sl_e0 = st.sidebar.number_input('Endpoint energy', min_value=0., max_value=20., value=1., step=0.001, help="Set the endpoint energy in MeV")
+    sl_a = st.sidebar.number_input('Mass number', min_value=sl_z, max_value=120, step=1, value=int(stableA(sl_z)), help="Set the mass number of the initial state")
+    sl_r = st.sidebar.number_input('Radius', min_value=0.01, max_value=100., step=0.1, value=approxNuclRadius(sl_z,sl_a-sl_z), help="Set the rms nuclear radius in fm")
 
     z = int(sl_z)
     a = int(sl_a)
-    e0 = float(sl_e0)
     r = float(sl_r)*(5./3.)**0.5*1e-15/constants.NATURALLENGTH
 
 else:
     dfAME = downloadAME()
 
-    dfAME
+    str_iso = st.sidebar.text_input('Isotope', value='6He')
 
-    str_iso = st.sidebar.text_input('Isotope', placeholder='Ex. 6He')
+    m = re.search(r'\d+', str_iso)
+    if m:
+        a = int(m.group(0))
+        z = constants.atoms.index(str(str_iso).replace(m.group(0), '').strip())
+    else:
+        st.error('Not a valid isotope name. Expecting something like 6He or 45Ca.')
 
-    z = 2
-    a = 6
-    e0 = 3.5
-    e_step = 0.001
-    r = 0.01
+    sl_r = st.sidebar.number_input('Radius', min_value=0.01, max_value=100., step=0.1, value=approxNuclRadius(z,a-z), help="Set the rms nuclear radius in fm")
 
-sl_e_step = st.sidebar.number_input('Energy step', min_value=0.001, max_value=1., value=0.001, step=0.001)
-e_step = float(sl_e_step)
+    r = sl_r*(5./3.)**0.5*1e-15/constants.NATURALLENGTH
+
+    if beta_type == 'Beta-':
+        Qvalue = dfAME.loc[(dfAME['Z'] == z) & (dfAME['A'] == a), 'mass'].values[0]-dfAME.loc[(dfAME['Z'] == (z+1)) & (dfAME['A'] == a), 'mass'].values[0]
+    else:
+        Qvalue = dfAME.loc[(dfAME['Z'] == z) & (dfAME['A'] == a), 'mass'].values[0]-dfAME.loc[(dfAME['Z'] == (z-1)) & (dfAME['A'] == a), 'mass'].values[0]-2*constants.ELECTRON_MASS_C2
+
+sl_e0 = st.sidebar.number_input('Endpoint energy', min_value=0., max_value=20e3, value=Qvalue, step=1., help="Set the endpoint energy in keV")
+sl_e_step = st.sidebar.number_input('Energy step', min_value=0.1, max_value=1000., value=1., step=1., help="Set the step energy in keV")
 
 @st.cache
 def calculateSpectrum(Z, A, R, E0, E_step):
@@ -111,47 +139,54 @@ def convert_df(df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df.to_csv(index=False).encode('utf-8')
 
+st.subheader('Transition info')
 
-df = calculateSpectrum(z, a, r, e0, e_step)
+st.write("""
+Current transition information:
+""")
 
-st.subheader('Electron spectrum')
+if sl_e0 > 0:
+    zeff = z if beta_type == 'Beta-' else -z
+    df = calculateSpectrum(zeff, a, r, sl_e0, sl_e_step)
 
-p = figure(title="Electron spectrum", x_axis_label='Kinetic energy [MeV]', y_axis_label='dN/dE')
+    st.subheader('Electron spectrum')
 
-p.line(df['Energy'], df['Spectrum'], legend_label='All corrections', line_width=2, muted_alpha=0.2, color=Category10[3][0])
-p.line(df['Energy'], df['PhaseSpace'], legend_label='Phase Space', line_width=2, muted_alpha=0.2, color=Category10[3][1])
+    p = figure(title="Electron spectrum", x_axis_label='Kinetic energy [keV]', y_axis_label='dN/dE')
 
-p.legend.click_policy = "mute"
+    p.line(df['Energy'], df['Spectrum'], legend_label='All corrections', line_width=2, muted_alpha=0.2, color=Category10[3][0])
+    p.line(df['Energy'], df['PhaseSpace'], legend_label='Phase Space', line_width=2, muted_alpha=0.2, color=Category10[3][1])
 
-st.bokeh_chart(p, use_container_width=True)
+    p.legend.click_policy = "mute"
 
-csv = convert_df(df)
+    st.bokeh_chart(p, use_container_width=True)
 
-st.download_button(
-    label="Download data as CSV",
-    data=csv,
-    file_name='spectrum.csv',
-    mime='text/csv',
-)
+    csv = convert_df(df)
 
+    st.download_button(
+        label="Download data as CSV",
+        data=csv,
+        file_name='spectrum.csv',
+        mime='text/csv',
+    )
 
-# -- Notes on whitening
-with st.expander("More details"):
-    st.markdown("""
+    # -- Notes on whitening
+    with st.expander("More details"):
+        st.markdown("""
  You are looking at the probability distribution for the kinetic energy of the beta particle after (nuclear) beta decay.
  The different corrections can be plotted individually by clicking the elements in the legend
 """)
-    if st.checkbox('Show individual corrections'):
-        pcorr = figure(title='Corrections', x_axis_label='Kinetic energy [MeV]', y_axis_label='Correction', y_range=DataRange1d(only_visible=True))
-        i = 0
-        dfCorr = df[['FermiFunction', 'L0', 'U', 'RadiativeCorrections']]
-        for column in dfCorr:
-            pcorr.line(df['Energy'], df[column], legend_label=column, color=Category10[len(dfCorr.columns)][i], line_width=2)
-            i+=1
-        pcorr.legend.click_policy = "hide"
+        if st.checkbox('Show individual corrections'):
+            pcorr = figure(title='Corrections', x_axis_label='Kinetic energy [keV]', y_axis_label='Correction', y_range=DataRange1d(only_visible=True))
+            i = 0
+            dfCorr = df[['FermiFunction', 'L0', 'U', 'RadiativeCorrections']]
+            for column in dfCorr:
+                pcorr.line(df['Energy'], df[column], legend_label=column, color=Category10[len(dfCorr.columns)][i], line_width=2)
+                i+=1
+            pcorr.legend.click_policy = "hide"
 
-        st.bokeh_chart(pcorr, use_container_width=True)
+            st.bokeh_chart(pcorr, use_container_width=True)
 
-    if st.checkbox('Show raw data'):
-        df
-
+        if st.checkbox('Show raw data'):
+            df
+else:
+    st.error("Endpoint energy is less than 0. Can't calculate spectrum.")
